54969721af6f2599404b18d2f69878bc5055c2c3
julendiiez <julendiez@opendeusto.es>
2023-05-22 14:06:38 +0200

    Anadir camara

--- index.py
+++ index.py
@@ -1,30 +1,36 @@
-#aqui se definen las rutas de nuestra pagina
+import cv2
+from flask import Flask, render_template, Response
 
-from flask import *
-import datetime
+app = Flask(__name__)
 
-app=Flask(__name__)
+# Capturar video desde la cámara USB
+def capture_camera():
+    camera = cv2.VideoCapture(1)  # 0 indica el primer dispositivo de video (cámara)
+    while True:
+        # Leer el cuadro de video
+        success, frame = camera.read()
+        if not success:
+            break
+        else:
+            # Codificar el cuadro de video en formato de imagen JPEG
+            ret, jpeg = cv2.imencode('.jpg', frame)
+            frame = jpeg.tobytes()
 
-@app.route('/')#Se llama a la funcion sobre la url principal
+            # Generar un generador de secuencia de bytes para el cuadro de video
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
-def home():
+    camera.release()
+
+
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(capture_camera(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/')
+def camara():
     return render_template('inicio.html')
 
-
-@app.route('/camara')
-
-def camara():
-    return render_template('camara.html')
-
-def datos():
-    data={
-        "hora":datetime.datetime.now(),
-        "asignatura":"ACO"
-
-    }
-    return render_template('datos.html',**data)
-
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=8000)
-
+    app.run(host='0.0.0.0', port=5000, debug=True)
